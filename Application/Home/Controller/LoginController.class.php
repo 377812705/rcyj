c50e38485f97fe301e41a7f47d0108562654774a
bohemai <tianxiaoguang03@163.com>
2015-08-30 11:31:59 +0800

    第三方登录

--- Application/Home/Controller/LoginController.class.php
+++ Application/Home/Controller/LoginController.class.php
@@ -3,8 +3,6 @@ namespace Home\Controller;
 
 use User\Api\UserApi;
 use Vendor\sms\SendSMS;
-use Vendor\weibo\SaeTOAuthV2;
-
 class LoginController extends HomeController
 {
 
@@ -221,37 +219,69 @@ class LoginController extends HomeController
         $this->redirect("Index/index");
     }
 
-    /**
-     * 新浪微博登录
-     */
-    public function sinalogin()
-    {
-        $o = new SaeTOAuthV2("2443143300", "a4dbd21f4f7467d41e4bac092863de42");
+    public function thridlogin($type = null) {
+        empty($type) && $this->error('参数错误');
+        import('Org.ThinkSDK.ThinkOauth');
 
-        if (isset($_REQUEST['code'])) {
-            $keys = array();
-            $keys['code'] = $_REQUEST['code'];
-            $keys['redirect_uri'] = "http://www.2ciyuanjie.com";
-            try {
-                $token = $o->getAccessToken('code', $keys);
-            } catch (OAuthException $e) {
-            }
-        }
 
-        if ($token) {
-            $_SESSION['token'] = $token;
-            setcookie('weibojs_' . $o->client_id, http_build_query($token));
-            $this->redirect("Index/index");
-        } else {
-            $this->redirect("Login/login");
-        }
+        $sns = \ThinkOauth::getInstance($type);
+//echo $sns->getRequestCodeURL();exit;
+//        //跳转到授权页面
+        redirect($sns->getRequestCodeURL());
     }
 
-    /**
-     * QQ登录
-     */
-    public function qqlogin()
-    {
+    public function callback($type = null, $code = null) {
+        header("Content-type: text/html; charset=utf-8");
+        (empty($type) || empty($code)) && $this->error('参数错误');
+        import('Org.ThinkSDK.ThinkOauth');
+        $sns = \ThinkOauth::getInstance($type);
 
+        //腾讯微博需传递的额外参数
+        $extend = null;
+        if ($type == 'tencent') {
+            $extend = array('openid' => $this->_get('openid'), 'openkey' => $this->_get('openkey'));
+        }
+        $tokenArr = $sns->getAccessToken($code, $extend);
+        $openid = $tokenArr['openid'];
+        $token = $tokenArr['access_token'];
+        setSessionCookie("openid", $openid);
+        setSessionCookie("access_token", $token);
+//        $con = "openid:".$openid."\n"."token".$token;
+//	file_put_contents("1.txt", $con);
+        //获取当前登录用户信息
+        if ($openid) {
+            $field = strtolower($type);
+            setSessionCookie("field", $field);
+            $userinfo = M("thirduser")->field('id,name')->where("" . $field . "= '" . $openid . "'")->find();
+
+            if ($userinfo) { //若是有该账号就登录
+                setSessionCookie("userid", $userinfo['id']);
+                setSessionCookie("username", $userinfo['name']);
+                echo "<script>document.location.href='" . __APP__ . "';</script>";
+                exit;
+            } else { //没有的话绑定
+                $userid = getSessionCookie('userid');
+                $username = getSessionCookie('username');
+                if ($userid != '' && $username != '') { //用户已登录，自动绑定
+                    //绑定账号
+                    M('user')->where("id = " . $userid . "")->save(array($field => $openid));
+                    emptySessionCookie('type');
+                    emptySessionCookie('openid');
+                    $this->success("绑定成功！", "/");
+                } else { //用户未登录，跳转到绑定页面
+                    if ($filed == 'qq') { //针对新版qq互联在绑定页，要显示昵称，否则不通过***
+                        $data = $sns->call('user/get_user_info');
+                        $nickname = $data['nickname'];
+                    } else {
+                        $userinfo = A('Type', 'Event')->$type($tokenArr);
+                        $nickname = $userinfo['name'];
+                    }
+                    setSessionCookie('nickname', $nickname);
+                    $this->redirect("Index/bind");
+                }
+            }
+        } else {
+            echo "<script>alert('系统出错;请稍后再试！');document.location.href='" . __APP__ . "';</script>";
+        }
     }
 }

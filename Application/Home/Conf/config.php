791656ff09cbc92b049e504f00ef2e9cc5dd1d0e
bohemai <tianxiaoguang03@163.com>
2015-09-06 17:50:13 +0800

    首页布局问题，作者推荐头像左右应该还留出出血位，与设计效果不一致

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -11,7 +11,7 @@
  * 前台配置文件
  * 所有除开系统级别的前台配置
  */
-$SITE_URL = "http://www.erciyuanjie.com/";
+$SITE_URL = "http://www.2ciyuanjie.com/";
 define('URL_CALLBACK', "" . $SITE_URL . "Login/callback/type/");
 return array(
     //腾讯QQ登录配置
